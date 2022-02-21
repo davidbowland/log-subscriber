056d905df54d053acc9e781c61816e309e2b4466
David Bowland <dbowland1@gmail.com>
2022-02-21 06:15:56 -0600

    Add multi-AZ deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,11 @@ if [[ -z "$1" ]]; then
   $(./scripts/assumeDeveloperRole.sh)
 fi
 
+if [[ -z "$PHONE_NUMBER" ]]; then
+  echo "Variable PHONE_NUMBER not defined"
+  exit 1
+fi
+
 # Build from template
 
 SAM_TEMPLATE=template.yaml
@@ -14,13 +19,13 @@ sam build --template ${SAM_TEMPLATE} --use-container
 
 # Deploy build lambda to east-1
 
-TESTING_ARTIFACTS_BUCKET=aws-sam-cli-managed-log-subscribe-artifactsbucket-1vnmb3ax14y27
-TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/aws-sam-cli-managed-log-s-CloudFormationExecutionR-I1FE17UFE5C3"
+TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/dbowland-cloudformation-test"
 TESTING_STACK_NAME=log-subscriber-test
 sam deploy --stack-name ${TESTING_STACK_NAME} \
            --capabilities CAPABILITY_IAM \
            --region us-east-1 \
-           --s3-bucket ${TESTING_ARTIFACTS_BUCKET} \
+           --s3-bucket dbowland-lambda-east-1-test \
+           --s3-prefix log-subscriber-test \
            --no-fail-on-empty-changeset \
            --role-arn ${TESTING_CLOUDFORMATION_EXECUTION_ROLE} \
            --parameter-overrides "Environment=test PhoneNumber=$PHONE_NUMBER"
@@ -30,7 +35,8 @@ sam deploy --stack-name ${TESTING_STACK_NAME} \
 sam deploy --stack-name ${TESTING_STACK_NAME} \
            --capabilities CAPABILITY_IAM \
            --region us-east-2 \
-           --s3-bucket ${TESTING_ARTIFACTS_BUCKET} \
+           --s3-bucket dbowland-lambda-east-2-test \
+           --s3-prefix log-subscriber-test \
            --no-fail-on-empty-changeset \
            --role-arn ${TESTING_CLOUDFORMATION_EXECUTION_ROLE} \
            --parameter-overrides "Environment=test PhoneNumber=$PHONE_NUMBER"
